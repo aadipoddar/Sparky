2c4369326d40d5c4bc07c0a524bd96814f88f624
Aadi Poddar <aadipoddarmail@gmail.com>
2021-12-28 18:19:14 +0530

    Batch Renderer

--- Sparky-core/src/graphics/buffers/indexbuffer.h
+++ Sparky-core/src/graphics/buffers/indexbuffer.h
@@ -14,7 +14,8 @@ namespace sparky
 			GLuint m_Count;
 		public:
 			IndexBuffer(GLushort* data, GLsizei count);
-
+			IndexBuffer(GLuint* data, GLsizei count);
+			~IndexBuffer();
 			void bind() const;
 			void unbind() const;
 
